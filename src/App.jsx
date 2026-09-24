dd9e8be651916347079ea047d026d43f71e69b8b
Akash Ramjyothi <akash.ramjyothi@gmail.com>
2026-09-24 23:56:54 +0530

    Removed header component

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import { Opinions } from './components/Opinions';
 import { NewOpinion } from './components/NewOpinion';
 import { OpinionsContextProvider } from './store/opinions-context';
 
+
 function App() {
   return (
     <>
-      <Header />
       <main>
         <OpinionsContextProvider>
           <NewOpinion />
